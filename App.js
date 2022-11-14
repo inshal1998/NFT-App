1821c624063fd658db86bf43a7ee42bd786107bd
inshal1998 <inshalansari80@gmail.com>
2022-11-14 15:49:47 +0530

    fix: Assests And Navigation Route Inatalized

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { NavigationContainer,DefaultTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Detail, Home } from './app/screens';
+
+const Stack = createNativeStackNavigator();
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'rgb(255, 45, 85)',
+  },
+};
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer theme={MyTheme}>
+      <Stack.Navigator initialRouteName='Home' screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
